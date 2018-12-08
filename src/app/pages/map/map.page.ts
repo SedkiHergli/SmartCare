5878385faae9f156d7cf5c452bc9848865fb7c23
Sedki Hergli <herglisedki6@gmail.com>
2018-12-08 23:20:43 +0000

    log+rg-gard+map+loc

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
+
+
+declare var google;
 
 @Component({
   selector: 'app-map',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  constructor() { }
+  constructor(private geolocation: Geolocation, private plt: Platform) { }
 
   ngOnInit() {
+    this.ionViewDidLoad();
   }
+
+  ionViewDidLoad() {
+    this.plt.ready().then(() => { 
+      let mapOptions = {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+      this.geolocation.getCurrentPosition().then(pos => {
+        let latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        this.map.setCenter(latLng);
+        this.map.setZoom(16);
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    });
+  }
+ 
 
 }
